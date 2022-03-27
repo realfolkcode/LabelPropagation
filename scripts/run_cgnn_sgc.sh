7854bd0fa0fd9cf93dc972787c1d0518024fcef4
Dmitry Gavrilyev <64730991+realfolkcode@users.noreply.github.com>
2022-03-27 22:17:54 +0300

    Delete scripts directory

--- scripts/run_cgnn_sgc.sh
+++
@@ -1,1 +0,0 @@
-python sgc/train_cgnn.py --dataset election --gpu 0 --n-epochs 1000 --lr 0.1 #--bias
--- scripts/run_lp_sgc.sh
+++
@@ -1,1 +0,0 @@
-python sgc/train_lpgnn.py --dataset election --gpu 0 --n-epochs 300 --lr 0.1 --bias

d98078118448dcbafa0f99cee0a5f84983431e8e
Dmitry Gavrilyev <64730991+realfolkcode@users.noreply.github.com>
2022-03-28 09:54:55 +0300

    Add random util

--- utils.py
+++ utils.py
@@ -3,6 +3,17 @@ import scipy.sparse as sp
 import torch
 from sklearn.metrics import r2_score
 
+def set_random_seeds(seed_value=0, device='cpu'):
+    '''source https://forums.fast.ai/t/solved-reproducibility-where-is-the-randomness-coming-in/31628/5'''
+    np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
+    random.seed(seed_value)
+    if device != 'cpu':
+        torch.cuda.manual_seed(seed_value)
+        torch.cuda.manual_seed_all(seed_value)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
 def encode_onehot(labels):
     classes = set(labels)
     classes_dict = {c: np.identity(len(classes))[i, :] for i, c in
